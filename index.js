5bcc20d0e7c982d5e2b6ba82c7e2a9bb7b960793
Brad Coleman <bradcoleman60@gmail.com>
2023-01-25 06:50:26 -0800

    adds looping function to enter another employee

--- index.js
+++ index.js
@@ -12,123 +12,204 @@ inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
 //This is the object of the questions that will be answered in the terminal
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is the title of your project?",
-      validate(answer) {
-        if (answer.length < 1) {
-          return `Please enter a valid title`;
-        }
-        return true;
-      },
-    },
-    {
-      type: "input",
-      name: "author",
-      message: "Please enter your full name",
-      validate(answer) {
-        if (answer.length < 1) {
-          return `Please enter a valid author`;
-        }
-        return true;
-      },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter your email address?",
-      validate(answer) {
-        if (
-          answer == "" ||
-          answer.indexOf("@") == -1 ||
-          answer.indexOf(".") == -1
-        ) {
-          return "Please enter a valid email address";
-        }
-        return true;
-      },
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Please enter your Git Hub user name",
-    },
+const output =[];
 
-    {
-      type: "editor",
-      name: "description",
-      message: "Please describe your project",
+const managerQuestions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is the manager's name?",
+    validate(answer) {
+      if (answer.length < 1) {
+        return `Please enter a valid title`;
+      }
+      return true;
     },
-    {
-      type: "input",
-      name: "usage",
-      message: "Please describe the usage of your project",
+  },
+  {
+    type: "input",
+    name: "managerID",
+    message: "What is the manager's ID",
+    validate(answer) {
+      if (answer.length < 1) {
+        return `Please enter a valid author`;
+      }
+      return true;
     },
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "What is the manager's email address?",
+    validate(answer) {
+      if (
+        answer == "" ||
+        answer.indexOf("@") == -1 ||
+        answer.indexOf(".") == -1
+      ) {
+        return "Please enter a valid email address";
+      }
+      return true;
+    },
+  },
+];
 
-    {
-      type: "input",
-      name: "contribution",
-      message: "Please provide contribution guidelines",
-    },
+const employeeQuestions = [
+  {
+    type: "list",
+    name: "engOrIntern",
+    message: "Which type of employee do you want to enter?",
+    choices: ["Add an Engineer", "Add an Intern", "No, not at this time"],
+  },
 
-    {
-      type: "list",
-      name: "license",
-      message: "Please select a license you would like to use",
-      choices: ["MIT", "ISC", "Unlicense"],
-    },
+  //Engineer Questions
+  {
+    type: "input",
+    name: "engName",
+    message: "Please Engineer's Name:",
+    when: (answers) => answers.engOrIntern === "Add an Engineer",
+  },
+  {
+    type: "input",
+    name: "engID",
+    message: "Please Engineer's ID:",
+    when: (answers) => answers.engOrIntern === "Add an Engineer",
+  },
+  {
+    type: "input",
+    name: "engEmail",
+    message: "Please Engineer's Email:",
+    when: (answers) => answers.engOrIntern === "Add an Engineer",
+  },
+  {
+    type: "input",
+    name: "engGithub",
+    message: "Please Engineer's GitHub UserName:",
+    when: (answers) => answers.engOrIntern === "Add an Engineer",
+  },
+  //Intern Questions
+  {
+    type: "input",
+    name: "intName",
+    message: "Please Intern's Name:",
+    when: (answers) => answers.engOrIntern === "Add an Intern",
+  },
+  {
+    type: "input",
+    name: "intID",
+    message: "Please Intern's ID:",
+    when: (answers) => answers.engOrIntern === "Add an Intern",
+  },
+  {
+    type: "input",
+    name: "intEmail",
+    message: "Please Intern's Email:",
+    when: (answers) => answers.engOrIntern === "Add an Intern",
+  },
+  {
+    type: "input",
+    name: "engSchool",
+    message: "Please Intern's School:",
+    when: (answers) => answers.engOrIntern === "Add an Intern",
+  },
+  {
+    type: 'confirm',
+    name: 'enterAnother',
+    message: 'Do you want to add another employee?'
+  }
+];
 
-    {
-      type: "input",
-      name: "testSteps",
-      message: "Enter tests steps to ensure this project is working",
-    },
-
-    {
-      type: "confirm",
-      name: "installationStepsNeeded",
-      message: "Would you like to add installation steps to your Readme?",
-    },
-    {
-      type: "input",
-      name: "installationSteps",
-      message: "Enter the installation step here:",
-      when: (answers) => answers.installationStepsNeeded === true,
-    },
-    {
-      type: "loop",
-      name: "installationSteps2",
-      message: "Would you like to add another installation step?",
-      when: (answers) => answers.installationStepsNeeded === true,
-      questions: [
-        {
-          type: "input",
-          name: "installationSteps2",
-          message: "Enter the installation step here:",
-        },
-      ],
-    },
-  ])
-  //This logs the answers
-  .then((answers) => {
-    //Using object destructuring made variables equal to the keys in the answer object
-    const {
-      title,
-      author,
-      email,
-      github,
-      description,
-      license,
-      installationSteps,
-      installationSteps2,
-      usage,
-      contribution,
-      testSteps,
-    } = answers;})
-
-
-
+function askManager() {
+  inquirer.prompt(managerQuestions).then((answers) =>{
+    output.push(answers);
+    askEmployee()
+     
     
+  } )
+}
+
+function askEmployee() {
+  inquirer.prompt(employeeQuestions).then((answers) =>{
+    output.push(answers);
+    if(answers.enterAnother){
+      askEmployee()
+    } else {output.join(', ')}
+    console.log(output) 
+  })
+}
+
+askManager()
+
+//   //This logs the answers
+//   .then((answers) => {
+//     //Using object destructuring made variables equal to the keys in the answer object
+//     const {
+//       title,
+//       author,
+//       email,
+//       github,
+//       description,
+//       license,
+//       installationSteps,
+//       installationSteps2,
+//       usage,
+//       contribution,
+//       testSteps,
+//     } = answers;
+
+//     //Deconstruct the installation steps to ensure that that only values are retrived
+//     var outputData2 = installationSteps2.map(Object.values);
+
+//     // Function used to create the installation steps that can be input into the template literal
+//     var installString = "";
+//     createInstallationString(outputData2);
+//     function createInstallationString(outputData2) {
+//       for (i = 0; i < outputData2.length; i++) {
+//         installString = installString + outputData2[i] + "\n";
+//       }
+//     }
+
+//     //Pass variables to the settestContent function
+//     settextContent(
+//       title,
+//       author,
+//       email,
+//       github,
+//       description,
+//       license,
+//       installationSteps,
+//       usage,
+//       contribution,
+//       testSteps,
+//       installString
+//     );
+//   });
+
+// /* This sets the content (via a template literal string) of the readme document*/
+// function settextContent(
+//   title,
+//   author,
+//   email,
+//   github,
+//   description,
+//   license,
+//   installationSteps,
+//   usage,
+//   contribution,
+//   testSteps,
+//   installString
+// ) {
+//   var theHtmlPage = `XXXXXXXXXXXXXX 
+  
+//   Please also check the GitHub Repositories at: https://github.com/${github}/`;
+
+//   writeReadme(theReadMe);
+// }
+
+// /*This function writes the readme document*/
+// function writeReadme(theReadMe) {
+//   fs.writeFile("SAMPLE_README.md", theReadMe, (err) => {
+//     if (err) throw err;
+//     console.log("The file is saved");
+//   });
+// }
